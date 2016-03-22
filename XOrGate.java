7076a06dc7108fc0bd56a81c917d6ccbf70670fb
Mark Waller <M_P_waller@hotmail.com>
2016-03-22 18:31:52 +0800

    Update XOrGate.java

--- XOrGate.java
+++ XOrGate.java
@@ -1,7 +1,11 @@
 package org.gradle.gates;
 
 public class XOrGate implements Gates {
+	
+	
 	public int evaluate(int a,int b){
+		//This could be shortened to be
+		// return (a==1 ^ b==1)? 1:0;
 		int c=(a==1 ^ b==1)? 1:0;
 		return c;
 	}
